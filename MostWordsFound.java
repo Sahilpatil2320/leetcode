f9779357bd7ef06108db2f2238ba845e9a017d32
Sahilpatil2320 <sahilpatil222005@gmail.com>
2026-02-18 21:59:56 +0530

    Add files via upload

---
+++ MostWordsFound.java
@@ -0,0 +1,13 @@
+class MostWordsFound {
+    public int mostWordsFound(String[] sentences) {
+        int maxLen = 0;
+
+        for(String currSent : sentences){
+            int currLen = currSent.split(" ").length;
+            if(maxLen <currLen){
+                maxLen = currLen;
+            }
+        }
+        return maxLen;
+    }
+}
