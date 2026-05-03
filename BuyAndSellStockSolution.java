e1c9516fcc344788aef611370ed7a3d39e7e63a1
Sahilpatil2320 <sahilpatil222005@gmail.com>
2026-05-03 12:25:34 +0530

    Best Time to Buy and Sell Stock - O(n) solution

---
+++ BuyAndSellStockSolution.java
@@ -0,0 +1,16 @@
+public class BuyAndSellStockSolution {
+    public int maxProfit(int[] prices) {
+        int buy = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        
+        for(int i = 0; i < prices.length; i++){
+            if(buy < prices[i]){
+                int profit = prices[i] - buy;
+                maxProfit = Math.max(profit, maxProfit);
+            }else{
+                buy = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+}
