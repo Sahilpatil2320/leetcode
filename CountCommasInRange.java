d1739b951fc72ea7598d11ae2b64f847d4f616f7
SAHIL PATIL <sahilpatil222005@gmail.com>
2026-09-08 10:20:29 +0530

    Solved Count Commas in Range using math

---
+++ CountCommasInRange.java
@@ -0,0 +1,13 @@
+public class CountCommasInRange {
+    public int countCommas(int n) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i >= 1000) {
+                count++;
+            }
+        }
+
+        return count;    
+    }
+}
