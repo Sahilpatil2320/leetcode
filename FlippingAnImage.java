7dcff9196fe0f55296829717c128d192727deff1
SAHIL PATIL <sahilpatil222005@gmail.com>
2026-09-09 22:17:08 +0530

    Solved Flipping an Image using two pointers

---
+++ FlippingAnImage.java
@@ -0,0 +1,22 @@
+public class FlippingAnImage {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for (int[] row : image) {
+
+            int left = 0;
+            int right = row.length - 1;
+
+            while (left <= right) {
+
+                int temp = row[left];
+
+                row[left] = 1 - row[right];
+                row[right] = 1 - temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        return image;
+    }
+}
