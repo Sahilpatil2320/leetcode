cc608833fabaf9b48c4dd36d2b5626b1fc2d792d
SAHIL PATIL <sahilpatil222005@gmail.com>
2026-09-17 18:49:58 +0530

    Add files via upload

---
+++ find-two-non-overlapping-sub-arrays-each-with-target-sum.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int minSumOfLengths(int[] arr, int target) {
+        int n = arr.length;
+        int INF = n + 1;
+
+        int[] best = new int[n];
+        for (int i = 0; i < n; i++) {
+            best[i] = INF;
+        }
+
+        int left = 0;
+        int sum = 0;
+        int ans = INF;
+
+        for (int right = 0; right < n; right++) {
+            sum += arr[right];
+
+            while (sum > target) {
+                sum -= arr[left];
+                left++;
+            }
+
+            if (sum == target) {
+                int len = right - left + 1;
+
+                // There is a previous non-overlapping subarray
+                if (left > 0 && best[left - 1] != INF) {
+                    ans = Math.min(ans, len + best[left - 1]);
+                }
+
+                best[right] = len;
+            }
+
+            // Carry forward the best previous answer
+            if (right > 0) {
+                best[right] = Math.min(best[right], best[right - 1]);
+            }
+        }
+
+        return ans == INF ? -1 : ans;
+    }
+}
